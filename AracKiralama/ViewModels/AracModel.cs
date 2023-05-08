18400440c78e747ba668b0d63bf7203c8df17a69
zelx1 <ezelhanevcil@gmail.com>
2023-05-08 18:51:58 +0300

    Add project files.

---
+++ AracKiralama/ViewModels/AracModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AracKiralama.ViewModels
+{
+    public class AracModel
+    {
+        public int aracId { get; set; }
+        public int aracKatId { get; set; }
+        public string aracModel { get; set; }
+        public string aracMarka { get; set; }
+        public string aracYıl { get; set; }
+        public Nullable<int> aracFiyat { get; set; }
+    }
+}
